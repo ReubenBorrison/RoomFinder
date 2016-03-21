cf7620209d7c77fee421e2a47b3cacc8d6aeccc7
reuben480@gmail.com <AL@ALOHA-PC.SRH.11008021.com>
2016-03-21 08:58:46 +0100

    slider added partially

--- www/js/index.js
+++ www/js/index.js
@@ -39,7 +39,7 @@
                     console.log("Response = " + r.response);
                     console.log("Sent = " + r.bytesSent);
                     links.push(uploadUrl+r.response);
-                console.log("Upload Successfull");
+                    console.log("Upload Successfull");
 
                 }
 
@@ -75,7 +75,40 @@
                                       "desc":"'+desc+'","lat":'+Number(latLonArray[0])+',"lon":'+Number(latLonArray[1])+'}}',
          
                     success:function(data,xhr,status){
-                    alert(data.data[0][0].data.id);
+                    alert("data submitted");
+                     // for(var i in data.data)
+                    // {
+                    //       $("#demo").append(data.data[i][0].data.name);
+                    // }
+                    
+                    },
+                    error:function(xhr,err,msg){
+                                console.log(xhr);
+                                console.log(err);
+                                console.log(msg);
+                    }
+                }); 
+                //adding image links to the db
+                $.ajaxSetup({
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader("Accept","application/json; charset=UTF-8");
+                        xhr.setRequestHeader("Authorization","Basic bmVvNGo6c3JoMTIzNA==");
+                        xhr.setRequestHeader("Content-Type", "application/json");
+                    }
+                });
+                $.ajax({
+                    url:"http://ec2-52-58-42-113.eu-central-1.compute.amazonaws.com/db/data/cypher",
+                    //accepts: "application/json; charset=UTF-8",
+                    type:"POST",
+                    dataType:"json",
+                     data: '{ "query" : "match(n) where n.id={id} \
+                                        create(n)-[rented:rented]->(k:room{roomid:{roomId},roomNum:{roomNum},price:{price},\
+                                        description:{desc},latitude:{lat},longitude:{lon}}) return n,k;", \
+                            "params": {"id":"'+id+'","roomId":"'+roomId+'","roomNum":"'+roomNum+'","price":"'+price+'",\
+                                      "desc":"'+desc+'","lat":'+Number(latLonArray[0])+',"lon":'+Number(latLonArray[1])+'}}',
+         
+                    success:function(data,xhr,status){
+                    alert("data submitted");
                      // for(var i in data.data)
                     // {
                     //       $("#demo").append(data.data[i][0].data.name);
